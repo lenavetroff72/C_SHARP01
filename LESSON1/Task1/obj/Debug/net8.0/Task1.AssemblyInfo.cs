31e19d45bc6f26d2cbb9c54a47b3a58229c1b943
Lena Vetroff <calma72@yandex.ru>
2024-03-04 22:29:39 +0300

    Initial commit

--- LESSON1/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ LESSON1/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+847c85992a976c0ca1470ca4e7780d270f68765f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3938bf433f3e7246deea494c0c6e251923869318")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
