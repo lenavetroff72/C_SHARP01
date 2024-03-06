a9d625f98258f21d2239f13d9acf03e5a1e9fa60
Lena Vetroff <calma72@yandex.ru>
2024-03-06 22:39:39 +0300

    Add Dz1

---
+++ LESSON1/Dz1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 1. Напишите программу, которая на вход принимает два числа,
+//и выдает какое число большее, а какое меньшее.
+
+int firstNumber = 12;
+int secondNumber = 10;
+
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine("первое чиcло больше");    
+}
+else if (secondNumber > firstNumber)
+{
+    Console.WriteLine("второе число больше");
+}
+else
+{
+    Console.WriteLine("числа равны");
+}
