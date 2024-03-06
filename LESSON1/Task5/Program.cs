a5a3e027381ad4994dfe154b0c94244edbe0695b
Lena Vetroff <calma72@yandex.ru>
2024-03-06 23:05:50 +0300

    new version Task5

--- LESSON1/Task5/Program.cs
+++ LESSON1/Task5/Program.cs
@@ -1,10 +1,11 @@
-﻿int n = 100;
+﻿int n = 5;
 int i = 1;
 
 while (i <= n)
 {
+    if((n % 2) == 0)
     //Console.WriteLine(i);
-    Console.Write(i);
-    Console.Write(' ');
-    i = i + 1;
+        Console.Write(i);
+        Console.Write(' ');
+        i = i + 1;
 }
