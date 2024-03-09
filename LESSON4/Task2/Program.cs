4c1240ab6094099972012a138d7f58150fdd3275
Lena Vetroff <calma72@yandex.ru>
2024-03-09 08:03:16 +0300

    Add Lesson4:Task2

---
+++ LESSON4/Task2/Program.cs
@@ -0,0 +1,58 @@
+﻿int[,] CreateMatrix(int rowCount, int columsCount)
+{
+   int[,] matrix = new int[rowCount, columsCount];
+
+Random rnd = new Random();
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = rnd.Next(1, 1000);
+    }
+} 
+return matrix;
+
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = CreateMatrix(3, 4);
+ShowMatrix(matrix);
+foreach(int e in matrix)
+{
+    if(IsInteresting(e) == true)
+    {
+        Console.WriteLine(e);
+    }
+}
+
+bool IsInteresting(int value)
+{
+    int SumOfDigits = GetSumOfDigits(value);
+    if(SumOfDigits % 2 == 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+int GetSumOfDigits(int value)
+{
+    int sum = 0;
+    while(value > 0)
+    {
+        sum = sum + value % 10;
+        value = value / 10;
+    }
+    return sum;
+}
